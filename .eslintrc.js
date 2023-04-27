217d487559b7eb5ace60973e76927f52b32f67d2
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-04-27 22:24:47 +0200

    Settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,8 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['prettier'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
   root: true,
@@ -17,9 +16,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 1,
+    'prettier/prettier': 0,
   },
 };
