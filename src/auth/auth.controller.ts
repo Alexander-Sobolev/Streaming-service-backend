1ebde65865aa4b829c2159dd5b904d49b08e459e
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-05-30 07:16:18 +0200

    Create login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,6 +8,13 @@ constructor(private readonly AuthService: AuthService) {}
 
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
+  @Post('login')
+  async login(@Body() dto: AuthDto) {
+    return this.AuthService.login(dto);
+    }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
   @Post('register')
   async register(@Body() dto: AuthDto) {
     return this.AuthService.register(dto);
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 import { InjectModel } from 'nestjs-typegoose';
+import { hash, genSalt, compare } from 'bcryptjs';
 import { UserModel } from 'src/user/user.model';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,8 +11,34 @@ export class AuthService {
     @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
   ){}
 
-  async register(dto: any) {
-    const newUser = await this.UserModel.create(dto)
+  async login(dto: AuthDto) {
+    return await this.validateUser(dto) 
+  }
+
+  async register(dto: AuthDto) {
+    const oldUser = await this.UserModel.findOne({email: dto.email})
+    if (oldUser) {
+      throw new BadRequestException('User already exist')
+    }
+
+    const salt = await genSalt(10)
+
+    const newUser = await this.UserModel.create({
+      email: dto.email,
+      password: await hash(dto.password, salt)
+    })
     return newUser.save()
+  }
+
+  async validateUser(dto: AuthDto): Promise<UserModel> {
+    const user = await this.UserModel.findOne({email: dto.email})
+    if (!user) {
+      throw new UnauthorizedException('User not found')
+    }
+
+    const isValidPassword = await compare(dto.password, user.password)
+    if (!isValidPassword) throw new BadRequestException('Wrong password')
+
+    return user
   }
 }
